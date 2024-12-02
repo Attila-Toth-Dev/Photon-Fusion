1b75f216327d9b1a43f9c37dd336c715c33de090
Attila Toth <attilatwork@gmail.com>
2024-12-02 21:17:29 +1100

    ADDED: added project as well as basic netcoded player controller into the game.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+using Fusion;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+    private NetworkCharacterController _characterController;
+
+    private void Awake()
+    {
+        _characterController = GetComponent<NetworkCharacterController>();
+    }
+    
+    public override void FixedUpdateNetwork()
+    {
+        if (GetInput(out NetworkInputData data))
+        {
+            data.direction.Normalize();
+            _characterController.Move(5 * data.direction * Runner.DeltaTime);
+        }
+    }
+}
